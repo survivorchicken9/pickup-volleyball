83a6ac9fcbc8bc153dab4404e82079ee865602a3
chrishents <chrishentschel2002@gmail.com>
2022-09-24 19:28:09 +0200

    Included all flask requirements

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, session, flash, redirect
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
