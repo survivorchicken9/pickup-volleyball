5da275048bfd2b2744e9b1e835984a8baa6a8f02
survivorchicken9 <byronpauldolon@hotmail.com>
2022-10-23 18:54:01 +0200

    Implementing flask login but it's broken wooops

--- models/admin.py
+++ models/admin.py
@@ -10,6 +10,21 @@ class Admin:
     password: str
     _id: str = field(default_factory=lambda: uuid.uuid4().hex)
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymouse(self):
+        return False
+
+    def get_id(self) -> str:
+        return str(Database.find_one(self.collection, {"username": self.username})["_id"])
+
     @classmethod
     def get_all_admins(cls):
         return [cls(**elem).json() for elem in Database.find(cls.collection, {})]
